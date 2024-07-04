9305ebbe39cd302566e213b3df997225bbc772a1
Nick <38154033+kree-nickm@users.noreply.github.com>
2024-07-04 05:19:37 +0000

    Remember settings

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+const paramProps = ["icon","font","color","speed","yOffset","width","size","delay","delayVary","rotate"];
+  
 document.forms.params.elements.create.addEventListener("click", event => {
   event.preventDefault();
   
   window.params = {};
-  let paramProps = ["icon","font","color","speed","yOffset","width","size","delay","delayVary","rotate"];
   let paramList = [];
   for(let param of paramProps)
   {
@@ -21,6 +22,7 @@ document.forms.params.elements.create.addEventListener("click", event => {
   setParams();
   
   document.forms.params.elements.url.value = "https://kree-nickm.github.io/falling-stuff/source.html?" + paramList.join("&");
+  window.localStorage.setItem("params", JSON.stringify(window.params));
   createItems();
   document.forms.params.elements.preview.disabled = false;
 });
@@ -32,3 +34,17 @@ document.forms.params.elements.preview.addEventListener("click", event => {
   document.forms.params.elements.preview.disabled = true;
   startFalling();
 });
+
+let params = window.localStorage.getItem("params");
+if(params)
+{
+  params = JSON.parse(params);
+  if(params && typeof(params) == "object")
+  {
+    window.params = params;
+    for(let param of paramProps)
+      if(window.params[param] != document.forms.params.elements[param].placeholder)
+        document.forms.params.elements[param].value = window.params[param];
+    document.forms.params.elements.names.value = (window.params.names??[]).join(",");
+  }
+}
